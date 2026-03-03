b5ed87166662ce64f1bde9f881c6b91420bb6ae0
Amartya <amartya.kumar2001@gmail.com>
2026-03-03 11:55:05 +0530

    Completed shaadi-rsvp

--- src/06-shaadi-rsvp.js
+++ src/06-shaadi-rsvp.js
@@ -47,16 +47,50 @@
  */
 export function processGuests(guests, filterFn) {
   // Your code here
+
+  if (!Array.isArray(guests) || typeof filterFn !== "function") {
+    return []
+  }
+
+  const guestList = guests.filter(filterFn)
+  return guestList
+
 }
 
 export function notifyGuests(guests, notifyCallback) {
   // Your code here
+
+  if (!Array.isArray(guests) || typeof notifyCallback !== "function") {
+    return []
+  }
+
+  return guests.map(notifyCallback)
 }
 
 export function handleRSVP(guest, onAccept, onDecline) {
   // Your code here
+  if (guest === null || typeof guest === "undefined" || typeof onAccept !== "function" || typeof onDecline !== "function") {
+    return null
+  }
+
+  if (guest["rsvp"] === "yes") {
+    return onAccept(guest)
+  } else if (guest["rsvp"] === "no") {
+    return onDecline(guest)
+  } else {
+    return null
+  }
+
+
 }
 
 export function transformGuestList(guests, ...transformFns) {
-  // Your code here
+  if (!Array.isArray(guests)) return []
+
+  return transformFns.reduce((acc, curr) => {
+
+    if (typeof curr !== "function") return acc
+    return curr(acc)
+
+  }, [...guests])
 }
